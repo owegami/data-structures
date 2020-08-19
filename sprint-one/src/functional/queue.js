9489a88a49c139893bccf2108e56822d1de432a9
Andrew Kilduff <a.l.kilduff@gmail.com>
2020-08-19 16:10:09 -0700

    Implement functional stack.js and queue.js

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,6 @@
 var Queue = function() {
   var someInstance = {};
+  var lengthCounter = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,12 +8,23 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    for ( var i = lengthCounter; i > 0; i--) {
+      storage[i] = storage[i - 1];
+    }
+    storage[0] = value;
+    lengthCounter++;
+    // we need to add 1 to all other keys?
   };
 
   someInstance.dequeue = function() {
+    var dequeuedVal = storage[lengthCounter - 1];
+    delete storage[lengthCounter - 1];
+    lengthCounter > 0 ? lengthCounter-- : lengthCounter = 0;
+    return dequeuedVal;
   };
 
   someInstance.size = function() {
+    return lengthCounter;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,37 @@
 var Stack = function() {
   var someInstance = {};
+  var lengthCounter = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
+  // {0: 'a', 1: 'b'}
+  // var accumulator = last key
+  someInstance.lengthCount = function() {
+    return lengthCounter;
+  };
 
   // Implement the methods below
   someInstance.push = function(value) {
+    //use the length to add one to the end
+    storage[lengthCounter] = value;
+    lengthCounter++;
   };
 
   someInstance.pop = function() {
+    var poppedVal = storage[lengthCounter - 1];
+    delete storage[lengthCounter - 1];
+    lengthCounter > 0 ? lengthCounter-- : lengthCounter = 0;
+    // console.log(poppedVal);
+    return poppedVal;
   };
 
   someInstance.size = function() {
+    // return Object.keys(storage).length;
+    // careful with use of .length
+    return lengthCounter;
   };
 
   return someInstance;
 };
+
+var dog = new Stack;
