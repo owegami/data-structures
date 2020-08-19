1274c53893d5c276e5ae583732d61186068b4197
Andrew Kilduff <a.l.kilduff@gmail.com>
2020-08-19 16:34:12 -0700

    Implement intro to functional shared stack.js

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,38 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  var storage = {};
+
+  extend(someInstance, stackMethods);
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var extend = function(obj, objProps) {
+  for (var key in objProps) {
+    obj[key] = objProps[key];
+    console.log('key', key, 'objProps[key]', objProps[key]);
+  }
+};
+
+var stackMethods = {
+  push: function(value) {
+    storage[lengthCounter] = value;
+    lengthCounter++;
+  },
+
+  pop: function() {
+    var poppedVal = storage[lengthCounter - 1];
+    delete storage[lengthCounter - 1];
+    lengthCounter > 0 ? lengthCounter-- : lengthCounter = 0;
+    return poppedVal;
+  },
+
+  size: function() {
+    return lengthCounter;
+  },
+  lengthCounter: 0
+};
 
 
